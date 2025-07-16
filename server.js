d780bdfe7b46dbb2a5b8788edbb588482acbb81b
RehaanAnjariaAgain <rehaan1099@gmail.com>
2025-07-16 17:04:05 -0400

    added firebaseadmin file

--- server.js
+++ server.js
@@ -14,23 +14,7 @@ const port = 3000;
 
 app.use(cors());
 
-// Initialize Firebase Admin SDK with the service account
-admin.initializeApp({
-  credential: admin.credential.cert({
-    "type": "service_account",
-    "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
-    "client_email": process.env.CLIENT_EMAIL,
-    "client_id": process.env.CLIENT_ID,
-    "auth_uri": process.env.AUTH_URI,
-    "token_uri": process.env.TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER,
-    "client_x509_cert_url": process.env.CLIENT_URL,
-    "universe_domain": "googleapis.com"
-  }),
-  storageBucket: process.env.STORAGE_BUCKET,
-});
+const admin = require('./firebaseadmin');
 
 const db = admin.firestore();
 
